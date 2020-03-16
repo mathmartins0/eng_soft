83671e531da7dd6effd0c368f6e0de53adea6821
mathmartins0 <56521105+mathmartins0@users.noreply.github.com>
2020-03-15 21:23:13 -0300

    Update calculo.js

--- calculo.js
+++ calculo.js
@@ -1,13 +1,13 @@
 function calcular() {
-    var num1 = Number(document.getElementById("num1").value);
-    var num2 = Number(document.getElementById("num2").value);
+    var numero1 = Number(document.getElementById("numero1").value);
+    var numero2 = Number(document.getElementById("numero2").value);
     var elemResult = document.getElementById("resultado");
 
 
     if (elemResult.textContent === undefined) {
-       elemResult.textContent = "O resultado é " + String(num1 + num2) + ".";
+       elemResult.textContent = "O resultado é " + String(numero1 + numero2) + ".";
     }
     else {
-       elemResult.innerText = "O resultado é " + String(num1 + num2) + ".";
+       elemResult.innerText = "O resultado é " + String(numero1 + numero2) + ".";
     }
 }
